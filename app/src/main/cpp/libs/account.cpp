6015ef8ba1d175a31abae700f53c05f7dd16d81c
YassineBolard <yassine.bolard@etu.univ-tours.fr>
2025-11-14 18:04:31 +0100

    UPDATE : Passage of C++ 17 into C++ 23

---
+++ app/src/main/cpp/libs/account.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yass on 11/14/25.
+//
+
+#include "account.hpp"
---
+++ app/src/main/cpp/libs/account.hpp
@@ -0,0 +1,8 @@
+//
+// Created by yass on 11/14/25.
+//
+
+#ifndef BUDGETAPP_ACCOUNT_HPP
+#define BUDGETAPP_ACCOUNT_HPP
+
+#endif //BUDGETAPP_ACCOUNT_HPP
