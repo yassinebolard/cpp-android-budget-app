e6fc418cf9f957373aec3ed71e243c3b57a5c692
YassineBolard <yassine.bolard@etu.univ-tours.fr>
2025-11-14 18:25:18 +0100

    Add Account class

--- app/src/main/cpp/libs/account.cpp
+++ app/src/main/cpp/libs/account.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "account.hpp"
+
+void Account::deposit(int64_t amount) noexcept {
+    int64_t balance = this->getBalance();
+    balance = balance + amount;
+
+    this->set_balance(balance);
+};
+
+void Account::withdraw(int64_t amount) noexcept {
+    int64_t balance = this->getBalance();
+    balance = balance - amount;
+
+    this->set_balance(balance);
+};
--- app/src/main/cpp/libs/account.hpp
+++ app/src/main/cpp/libs/account.hpp
@@ -5,4 +5,43 @@
 #ifndef BUDGETAPP_ACCOUNT_HPP
 #define BUDGETAPP_ACCOUNT_HPP
 
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdint>
+
+class Account{
+    public:
+        enum class account_type_t : uint8_t {
+            CHECKING,
+            SAVINGS
+        };
+
+        const account_type_t DEFAULT_ACCOUNT_TYPE = account_type_t::CHECKING;
+
+    private:
+        /* Attributes */
+        std::string m_name;
+        int64_t m_balance;
+        account_type_t m_type{DEFAULT_ACCOUNT_TYPE};
+
+        /* Private functions to change values of attributes */
+        void set_name(std::string new_name) noexcept { this->m_name = std::move(new_name); };
+        void set_balance(int64_t new_balance) noexcept { this->m_balance = new_balance; };
+        void set_type(account_type_t new_type) noexcept { this->m_type = new_type; };
+
+    public:
+        /* Constructor / Destructor */
+        Account() = default;
+        explicit Account(std::string name, int64_t balance, account_type_t type) : m_name{std::move(name)}, m_balance{balance}, m_type{type} {};
+        virtual ~Account() = default;
+
+        [[nodiscard]] std::string getName() const noexcept { return this->m_name; };
+        [[nodiscard]] int64_t getBalance() const noexcept { return this->m_balance; };
+        [[nodiscard]] account_type_t getType() const noexcept { return this->m_type; };
+        void deposit(int64_t amount) noexcept;
+        void withdraw(int64_t amount) noexcept;
+};
+
 #endif //BUDGETAPP_ACCOUNT_HPP
