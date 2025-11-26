09868d9015efb52bf39a15901b9e501a7056ee07
YassineBolard <yassine.bolard@etu.univ-tours.fr>
2025-11-26 18:14:30 +0100

    Ajout des modifications

--- app/src/main/cpp/libs/budget_manager.hpp
+++ app/src/main/cpp/libs/budget_manager.hpp
@@ -23,20 +23,34 @@ public:
     explicit BudgetManager(std::map<std::string, Account> map) : m_accounts{std::move(map)} {};
     virtual ~BudgetManager() = default;
 
+    /**
+     * @brief Creates an account
+     */
     [[nodiscard]] bool createAccount(const std::string& init_name, Account::account_type_t init_type, int64_t initial_balance_cents);
 
     /**
      * @brief Gets a *modifiable* pointer to an account.
      */
     [[nodiscard]] Account* getAccount(const std::string& name) noexcept;
-
     /**
      * @brief Gets a *read-only* pointer to an account.
      */
     [[nodiscard]] const Account* getAccount(const std::string& name) const noexcept;
+    /**
+     * @brief Gets a vector of strings with all the account names.
+     */
     [[nodiscard]] std::vector<std::string> listAccountNames() const noexcept;
+    /**
+     * @brief Adds a transaction to a specific account using its name
+     */
     [[nodiscard]] bool addTransactionToAccount(const std::string& account_name, int64_t amount_cents, const std::string& description);
+    /**
+     * @brief Gets a vector of transactions with all the transactions from an account using its name
+     */
     [[nodiscard]] const std::vector<Transaction>& getTransactionsForAccount(const std::string& account_name) const;
+    /**
+     * @brief Gets the balance from a specific account using its name
+     */
     [[nodiscard]] int64_t getAccountBalance(const std::string& account_name) const noexcept;
 };
 
